1085ff8705ad56a5fbb0b4a28d0d396e427cb805
rodrigotarriba_m <rodrigotarriba@gmail.com>
2022-06-10 07:57:15 -0700

    Update - beginning U3L3

--- Assets/Scripts/CannonBall.cs
+++ Assets/Scripts/CannonBall.cs
@@ -1,14 +1,22 @@
-using System.Collections;
+    using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+
 
 public class CannonBall : MonoBehaviour
 {
     private Rigidbody m_cannonBallRigidBody;
-    private Animator m_cannonBallAnimator; 
+
+    [SerializeField] private Animator m_cannonBallAnimator;
+    [SerializeField] private float explosionRadius = 9.0f;
+    [SerializeField] private float explosionForce = 12.0f;
+    [SerializeField] private float explosionUpwardsModifier = 1.0f; //what does this do?
 
     private static readonly int exploded = Animator.StringToHash("Exploded");
     
+   
+
 
     private void Awake()
     {
@@ -18,6 +26,14 @@ public class CannonBall : MonoBehaviour
     }
 
 
+
+
+
+
+
+
+
+    // This method is called externally by the controller
     public void SetUp(Vector3 m_fireForce)
     {
         // Adding a impulse force to the cannonball
@@ -34,14 +50,43 @@ public class CannonBall : MonoBehaviour
         transform.rotation = Quaternion.FromToRotation(transform.up, collision.GetContact(0).normal) * transform.rotation;
 
 
-        //reset inertia, velocity, angular velocity etc = 0
+        // reset inertia, velocity, angular velocity etc = 0
         m_cannonBallRigidBody.velocity = Vector3.zero;
         m_cannonBallRigidBody.angularVelocity = Vector3.zero;
         m_cannonBallRigidBody.isKinematic = true;
 
 
-        //trigger the explosion
+        // trigger the explosion animation inside the animator
         m_cannonBallAnimator.SetTrigger(exploded);
+
+
+        // save the position of the explosion the moment we hit something
+        Vector3 explosionPosition = transform.position;
+
+        // get all the touching colliders
+        // OverlapSphere returns an array of all the colliders colliding with a specific sphere a moment in time - you assing a position, the radius of the circle and the layer mask that will trigger collisions.
+        Collider[] touchingColliders = Physics.OverlapSphere(explosionPosition, explosionRadius, LayerMask.GetMask("Targets"));
+
+        //apply force in a direction to the touching colliders
+        // we need an explosion that goes in the direction of all of the touching controllers
+        // Rigidbody.AddExposionForce -- adds a force to a rigidbody that simulates explosion effects
+
+        foreach (Collider collider in touchingColliders)
+        {
+            Rigidbody collidedRigidbody = collider.GetComponent<Rigidbody>();
+            if (collidedRigidbody != null)
+            {
+                collidedRigidbody.AddExplosionForce(explosionForce, explosionPosition, explosionRadius, explosionUpwardsModifier, ForceMode.Impulse);
+                /// Acceleartion - applies force over acceleration ignoring mass
+                /// Force - add continues force to the rigidbody, using its mass . f = m.a
+                /// Impulse - kinch impulse
+                /// Velocity change - slowing it down, ignoring its mass
+                /// If your collider is very small and you use a very hard force, you can breakthrough colliders.
+                
+               
+            }
+        }
+
 
 
     }
